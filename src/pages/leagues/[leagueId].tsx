4236754df5ec47235b2fbdc81c3b39d89ccc3661
vitorjaum <vitorjaum711@gmail.com>
2023-08-28 03:57:05 -0400

    feat: adds table on league page

--- src/pages/leagues/[leagueId].tsx
+++ src/pages/leagues/[leagueId].tsx
@@ -1,3 +1,5 @@
+import { Table } from "@/components/Table";
+
 type leagueProps = {
   name: string;
   id: string;
@@ -33,90 +35,12 @@ export async function getStaticProps(context: contextProps) {
   return { props: { standings: data.data } };
 }
 
-type teamProps = {
-  team: {
-    name: string;
-  };
-  stats: Array<statsProps>;
-};
-
-type statsProps = {
-  losses: number;
-  gamesPlayed: number;
-};
-
-type itemsInfo = {
-  name?: string;
-  logos?: string;
-  gamesPlayed: string;
-  losses: string;
-  pointDifferential: string;
-  points: string;
-  goalsAgainst: string;
-  goalsFor: string;
-  drawns: string;
-};
-
 export default function League({ standings }: any) {
-  const teamList = standings.standings;
-  // console.log(standings);
-  const itemsInfo: itemsInfo = {};
-  console.log({ teamList });
+  const standingsList = standings.standings;
 
-  console.log({ itemsInfo });
   return (
-    <>
-      <table>
-        <thead>
-          <tr>
-            <td>Name:</td>
-            <td>Games Played:</td>
-            <td>Losses:</td>
-            <td>Goal Difference:</td>
-            <td>points:</td>
-            <td>Goals Against:</td>
-            <td>Goals For:</td>
-            <td>Draws:</td>
-            <td>:</td>
-          </tr>
-        </thead>
-        <tbody>
-          {teamList.map((currTeam, idx) => {
-            const teamInfo = currTeam.team;
-            const teamStats = currTeam.stats;
-            itemsInfo.name = teamInfo.name;
-            itemsInfo.logos = teamInfo.logos;
-
-            teamStats.map((stats) => {
-              itemsInfo[stats.name as keyof itemsInfo] = stats.value;
-            });
-
-            const {
-              gamesPlayed,
-              losses,
-              pointDifferential,
-              points,
-              goalsAgainst,
-              goalsFor,
-              drawns,
-              name,
-            } = itemsInfo;
-
-            return (
-              <tr key={idx}>
-                <td>{name}</td>
-                <td>{gamesPlayed}</td>
-                <td>{losses}</td>
-                <td>{pointDifferential}</td>
-                <td>{points}</td>
-                <td>{goalsAgainst}</td>
-                <td>{goalsFor}</td>
-                <td>{drawns}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </>
+    <div>
+      <Table standings={standingsList} />
+    </div>
   );
 }
