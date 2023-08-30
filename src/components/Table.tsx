8fe5afaca61e3f168e915afc43b787b39165e97c
vitorjaum <vitorjaum711@gmail.com>
2023-08-30 02:17:22 -0400

    adds clubs images + Table component css

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,9 +1,10 @@
-import styles from "../styles/LeaguePage.module.css";
+import Image from "next/image";
+import styles from "../styles/Table.module.css";
 
 interface standingsProps {
   standings: [
     {
-      team: { name: string };
+      team: { name: string; logos: [{ href: string }] };
       stats: [{ name: string; value: string | number }];
     }
   ];
@@ -14,20 +15,21 @@ export function Table({ standings }: standingsProps) {
     <table className={styles.table}>
       <thead>
         <tr>
-          <td></td>
+          <td>#</td>
           <td>Name:</td>
           <td>Points:</td>
           <td>Games Played:</td>
-          <td>Losses:</td>
-          <td>Goal Difference:</td>
-          <td>Goals Against:</td>
-          <td>Goals For:</td>
+          <td>Wins:</td>
           <td>Draws:</td>
+          <td>Losses:</td>
+          <td>Goals:</td>
+          <td>Points Differential:</td>
         </tr>
       </thead>
       <tbody>
         {standings.map((club, idx) => {
           const name = club.team.name;
+          const logos = club?.team?.logos?.[0]?.href ?? "";
           const stats = club.stats;
           const itemsInfo: Record<string, string | number> = {};
           stats.map((item) => (itemsInfo[item.name] = item.value));
@@ -40,19 +42,30 @@ export function Table({ standings }: standingsProps) {
             pointsFor,
             ties,
             rank,
+            wins,
           } = itemsInfo;
 
           return (
             <tr key={idx}>
               <td>{rank}</td>
-              <td>{name}</td>
+              <td>
+                <Image
+                  alt={`logo of ${name}`}
+                  src={logos}
+                  width={20}
+                  height={20}
+                />
+                {name}
+              </td>
               <td>{points}</td>
               <td>{gamesPlayed}</td>
-              <td>{losses}</td>
-              <td>{pointDifferential}</td>
-              <td>{pointsAgainst}</td>
-              <td>{pointsFor}</td>
+              <td>{wins}</td>
               <td>{ties}</td>
+              <td>{losses}</td>
+              <td>
+                {pointsFor} : {pointsAgainst}
+              </td>
+              <td>{pointDifferential}</td>
             </tr>
           );
         })}
