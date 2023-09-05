f1b477d04aaaaf3408e093104ab89141281bb8bc
vitorjaum <vitorjaum711@gmail.com>
2023-09-05 09:12:33 -0400

    fix: head icon

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 import Header from "./Header";
 import styles from "../styles/Layout.module.css";
 import { Footer } from "./Footer";
+import Head from "next/head";
 
 interface LayoutProps {
   children: ReactNode;
@@ -10,6 +11,10 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
   return (
     <>
+      <Head>
+        <title>Country Leagues</title>
+        <link rel="shortcut icon" href="../futIcon.png" />
+      </Head>
       <Header />
       <div className={styles.mainContainer}>{children}</div>
       <Footer />
