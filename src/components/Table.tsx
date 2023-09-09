e7614871d7159716bf7f5e9d2fdb627ea9f18da8
vitorjaum <vitorjaum711@gmail.com>
2023-09-09 11:18:25 -0400

    feat: changes tags for abbreviations + styles table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -14,22 +14,22 @@ export function Table({ standings }: standingsProps) {
   return (
     <table className={styles.table}>
       <thead>
-        <tr>
-          <td>#</td>
-          <td className={styles.club_column}>Name</td>
-          <td>Points</td>
-          <td>Games Played</td>
-          <td>Wins</td>
-          <td>Draws</td>
-          <td>Losses</td>
-          <td>Goals</td>
-          <td>Points Differential</td>
+        <tr className={styles.header_row}>
+          <th>#</th>
+          <th className={styles.club_column}>Club</th>
+          <th>Pts</th>
+          <th>GP</th>
+          <th>Wins</th>
+          <th>Draws</th>
+          <th>Losses</th>
+          <th>Goals</th>
+          <th>PD</th>
         </tr>
       </thead>
       <tbody>
         {standings.map((club, idx) => {
           const name = club.team.name;
-          const logos = club?.team?.logos?.[0]?.href ?? "";
+          const logos = club?.team?.logos?.[0]?.href ?? "/futicon.png";
           const stats = club.stats;
           const itemsInfo: Record<string, string | number> = {};
           stats.map((item) => (itemsInfo[item.name] = item.value));
@@ -52,8 +52,8 @@ export function Table({ standings }: standingsProps) {
                 <Image
                   alt={`logo of ${name}`}
                   src={logos}
-                  width={20}
-                  height={20}
+                  width={30}
+                  height={30}
                 />
                 {name}
               </td>
