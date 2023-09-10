8d41f329c67ca8b249dbb4aa3dc8037085a224f1
vitorjaum <vitorjaum711@gmail.com>
2023-09-09 22:56:54 -0400

    feat: adds div with abbreviation meaning

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -12,64 +12,69 @@ interface standingsProps {
 
 export function Table({ standings }: standingsProps) {
   return (
-    <table className={styles.table}>
-      <thead>
-        <tr className={styles.header_row}>
-          <th>#</th>
-          <th className={styles.club_column}>Club</th>
-          <th>Pts</th>
-          <th>GP</th>
-          <th>Wins</th>
-          <th>Draws</th>
-          <th>Losses</th>
-          <th>Goals</th>
-          <th>PD</th>
-        </tr>
-      </thead>
-      <tbody>
-        {standings.map((club, idx) => {
-          const name = club.team.name;
-          const logos = club?.team?.logos?.[0]?.href ?? "/futicon.png";
-          const stats = club.stats;
-          const itemsInfo: Record<string, string | number> = {};
-          stats.map((item) => (itemsInfo[item.name] = item.value));
-          const {
-            points,
-            losses,
-            gamesPlayed,
-            pointDifferential,
-            pointsAgainst,
-            pointsFor,
-            ties,
-            rank,
-            wins,
-          } = itemsInfo;
+    <div className={styles.table_container}>
+      <table className={styles.table}>
+        <thead>
+          <tr className={styles.header_row}>
+            <th>#</th>
+            <th className={styles.club_column}>Club</th>
+            <th>Pts</th>
+            <th>GP</th>
+            <th>Wins</th>
+            <th>Draws</th>
+            <th>Losses</th>
+            <th>Goals</th>
+            <th>PD</th>
+          </tr>
+        </thead>
+        <tbody>
+          {standings.map((club, idx) => {
+            const name = club.team.name;
+            const logos = club?.team?.logos?.[0]?.href ?? "/futicon.png";
+            const stats = club.stats;
+            const itemsInfo: Record<string, string | number> = {};
+            stats.map((item) => (itemsInfo[item.name] = item.value));
+            const {
+              points,
+              losses,
+              gamesPlayed,
+              pointDifferential,
+              pointsAgainst,
+              pointsFor,
+              ties,
+              rank,
+              wins,
+            } = itemsInfo;
 
-          return (
-            <tr key={idx}>
-              <td>{rank}</td>
-              <td className={styles.club_column}>
-                <Image
-                  alt={`logo of ${name}`}
-                  src={logos}
-                  width={30}
-                  height={30}
-                />
-                {name}
-              </td>
-              <td>{points}</td>
-              <td>{gamesPlayed}</td>
-              <td>{wins}</td>
-              <td>{ties}</td>
-              <td>{losses}</td>
-              <td>
-                {pointsFor} : {pointsAgainst}
-              </td>
-              <td>{pointDifferential}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+            return (
+              <tr key={idx}>
+                <td>{rank}</td>
+                <td className={styles.club_column}>
+                  <Image
+                    alt={`logo of ${name}`}
+                    src={logos}
+                    width={30}
+                    height={30}
+                  />
+                  {name}
+                </td>
+                <td>{points}</td>
+                <td>{gamesPlayed}</td>
+                <td>{wins}</td>
+                <td>{ties}</td>
+                <td>{losses}</td>
+                <td>
+                  {pointsFor} : {pointsAgainst}
+                </td>
+                <td>{pointDifferential}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <div className={styles.abbreviations_meaning}>
+        <p>pts: points</p> <p>GP: Games Played</p> <p>PD: Points Difference</p>
+      </div>
+    </div>
   );
 }
